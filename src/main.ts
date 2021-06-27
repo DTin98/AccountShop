77505b8a9f41be036ed76ac5b862a7fb1f7e0e96
DTin98 <59561408+DTin98@users.noreply.github.com>
2021-06-27 14:19:01 +0700

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from './shared/pipes/validation.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true } );
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
