ded22f5b72d35f4a2de77cc387bf43d334e00345
Tin98 <kaisin1505@gmail.com>
2021-07-17 20:13:36 +0700

    fix: pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,23 +2,10 @@ module.exports = {
   apps: [
     {
       name: "AccountShop",
-      script: "npm start",
+      script: "./dist/main.js",
       exec_mode: "cluster_mode",
       instances: 2,
       autorestart: true,
     },
   ],
-  deploy: {
-    production: {
-      host: "0.0.0.0",
-      user: "deploy",
-      ssh_options: ["ForwardAgent=yes"],
-      ref: "origin/master",
-      repo: "git@github:repo/repo.git",
-      path: "/path/to/project",
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-  },
 };
