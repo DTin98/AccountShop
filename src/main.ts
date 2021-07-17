543c189f2d39dd07258090d1b7b495399a1cf5a3
Tin98 <kaisin1505@gmail.com>
2021-07-17 23:49:29 +0700

    fix deploy

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup("", app, document);
 
   await app.listen(8080);
 }
